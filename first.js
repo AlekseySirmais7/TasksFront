f909af1e2b85f9eef906eec620d8e3c9d701c49b
Aleksey <aleksei.sirmais@yandex.ru>
2020-03-07 23:52:58 +0300

    3 ways try

--- first.js
+++ first.js
@@ -1,14 +1,62 @@
 // Реализовать метод `.delay`
 // ...
 
-function foo() {
-    console.log("Wow!");
+
+function Foo() {
+
+    const callbackFunc = () => {
+        console.log("Wow! 1");
+    };
+
+    callbackFunc.delay = (time) => {
+        setTimeout(callbackFunc, time);
+    };
+
+    return callbackFunc
 }
 
-foo.delay = (time) => {
-    setTimeout(foo, time);
+Foo()();
+
+const a = new Foo();
+
+a.delay(1000);
+
+a();
+
+
+
+// ============= еще один вариант
+function Foo2() {
+    return console.log("Wow! 2");
+}
+
+Foo2.prototype.delay = (time) => {
+    setTimeout(Foo2, time);
 };
 
-foo.delay(300);
+const b = new Foo2(); // Wow 2
 
-foo.delay(3000);
+b.delay(1000); // Wow 2
+
+Foo2(); // Wow 2
+
+
+
+// =============== вариант 3
+function Foo3() {
+    this.standart = () => {
+        console.log("Wow! 3");
+    };
+}
+
+Foo3.prototype = {
+    delay:function(time) {
+        setTimeout(this.standart, time);
+    }
+};
+
+var c  = new Foo3();
+
+c.delay(2000);
+
+c.standart();
