b13505ad2645ea2a2354566cd6d90d521c2ac520
Aleksey <aleksei.sirmais@yandex.ru>
2020-05-29 22:52:56 +0300

    tasks

--- first.js
+++ first.js
@@ -1,62 +1,35 @@
-// Реализовать метод `.delay`
-// ...
+// Параллельные вычисления
 
+function parallel(callbacks, final) {
 
-function Foo() {
-
-    const callbackFunc = () => {
-        console.log("Wow! 1");
-    };
-
-    callbackFunc.delay = (time) => {
-        setTimeout(callbackFunc, time);
-    };
-
-    return callbackFunc
-}
-
-Foo()();
-
-const a = new Foo();
-
-a.delay(1000);
-
-a();
-
-
-
-// ============= еще один вариант
-function Foo2() {
-    return console.log("Wow! 2");
-}
-
-Foo2.prototype.delay = (time) => {
-    setTimeout(Foo2, time);
-};
-
-const b = new Foo2(); // Wow 2
-
-b.delay(1000); // Wow 2
-
-Foo2(); // Wow 2
-
-
-
-// =============== вариант 3
-function Foo3() {
-    this.standart = () => {
-        console.log("Wow! 3");
-    };
-}
-
-Foo3.prototype = {
-    delay:function(time) {
-        setTimeout(this.standart, time);
+    if (callbacks.length) {
+        callbacks.forEach((item, i) => {
+            if (item.length) {
+                callbacks[i] =  new Promise(item)
+            } else {
+                callbacks[i] = Promise.resolve( item() )
+            }
+        })
+        Promise.all(callbacks).then(final)
     }
-};
 
-var c  = new Foo3();
+}
 
-c.delay(2000);
 
-c.standart();
+parallel([
+    function (resolve) {
+        setTimeout(function () {
+            resolve(10);
+        }, 50);
+    },
+    function () {
+        return 5;
+    },
+    function (resolve) {
+        setTimeout(function () {
+            resolve(0);
+        }, 10)
+    }
+], function (results) {
+    console.log(results); // [10, 5, 0]
+});
--- second.js
+++ second.js
@@ -1,16 +1,67 @@
-/**
- * Найти пересечение двух массивов
- * @param  {number[]} left
- * @param  {number[]} right
- * @return {number[]}
- */
-function intersection(left, right) {
 
-    return Array.from(new Set(left)).filter(element => right.includes(element));
+// Реализовать класс Futures
+
+function Futures(executor) {
+
+    executor(
+        (value) => {
+            if (!this.status) {
+                this.status = "resolve";
+                this.value = value;
+                if (this.onResolve) {
+                    this.onResolve(this.value);
+                }
+            }
+        },
+
+        (value) => {
+            if (!this.status) {
+                this.status = "reject";
+                this.value = value;
+                if (this.onReject) {
+                    this.onReject(this.value);
+                }
+            }
+        },
+    );
 
 }
 
-console.log(intersection(
-    [1, 2, 3, 4, 5,1,1,1],
-    [2, 8, 3, 2]
-));
+Futures.prototype.then = function (onResolve, onReject) {
+    if (this.status) {
+        if (this.status === "resolve") {
+            onResolve(this.value);
+        } else {
+            onReject(this.value);
+        }
+    } else {
+        this.onResolve = onResolve;
+        this.onReject = onReject;
+    }
+};
+
+
+
+// Тест #1
+var foo = new Futures(function (resolve, reject) {
+    resolve(123);
+});
+
+foo.then(function (val) {
+    console.log("foo.resolved:", val === 123);
+}, function () {
+    console.log("foo.resolved: fail");
+});
+
+
+// Тест #2
+var bar = new Futures(function (resolve, reject) {
+    setTimeout(resolve.bind(null, "fail"), 300);
+    setTimeout(reject.bind(null, "ok"), 200);
+});
+
+bar.then(function () {
+    console.log("bar.rejected: fail");
+}, function (val) {
+    console.log("bar.rejected:", val === "ok");
+});
